82fd2e9e257caac22ae24c4b7b67a62aafd75458
Adeon18 <ostaptrush18@gmail.com>
2022-03-27 18:25:08 +0300

    Added thread actually not so safe deque

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+#include <iostream>
+#include <thread>
+#include "options_parser.h"
+#include "../include/mt_deque.hpp"
+
+int main(int argv, char* argc[]) {
+
+    mt_deque_t<int> mt_d;
+
+    std::thread t2(&mt_deque_t<int>::push_back, &mt_d, 2);
+    std::thread t3(&mt_deque_t<int>::push_front, &mt_d, 3);
+    std::thread t1(&mt_deque_t<int>::push_back, &mt_d, 1);
+
+//    std::thread t4(&mt_deque_t<int>::pop_back, &mt_d);
+    t2.join();
+    t3.join();
+    t1.join();
+//    t4.join();
+
+    // IT FUCKING WORKS!!!!!!!
+
+    auto size = mt_d.size();
+    for (size_t i = 0; i < size; i++) {
+        std::cout << mt_d.pop_back() << std::endl;
+    }
+
+	return 0;
+}
