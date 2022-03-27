128f58a52e9c1c4eb9b4e8434378d05c41ec1fc9
michael-2956 <work.michael.2956@gmail.com>
2022-03-27 22:52:43 +0300

    add file read

--- list_and_read/list_and_read.hpp
+++ list_and_read/list_and_read.hpp
@@ -1,14 +1,62 @@
 #ifndef _LIST_AND_READ_HPP
 #define _LIST_AND_READ_HPP
 
+#include "../include/mt_deque.hpp"
 #include <filesystem>
+#include <fstream>
+#include <deque>
 
 namespace fs = std::filesystem;
+
+typedef std::deque<char, std::allocator<char> > chardeque;
 
 void add_files_to_queue(mt_deque_t<fs::path>& mt_d, std::string indir) {
     for (auto const& dir_entry : fs::recursive_directory_iterator(indir)) {
         mt_d.push_back(dir_entry.path());
     }
+    mt_d.push_back(fs::path(""));
+}
+
+chardeque read_file_noseek(std::ifstream& fs_in) {
+    using std::begin;
+    using std::end;
+
+    // default buffer size of an ifstream
+    auto constexpr chunk_size = std::size_t{BUFSIZ};
+
+    auto container = std::deque<char>();
+
+    auto chunk = std::array<char, chunk_size>{};
+
+    while (
+        fs_in.read(chunk.data(), chunk.size()) || fs_in.gcount()
+    ) container.insert(
+        end(container), begin(chunk), begin(chunk) + fs_in.gcount()
+    );
+
+    return container;
+}
+
+void read_files_from_deque(mt_deque_t<fs::path>& mt_d_filenames, mt_deque_t<chardeque>& mt_d_file_contents) {
+    while (true) {
+        auto file_path = mt_d_filenames.pop_back();
+
+        if (file_path.empty()) {
+            break;
+        }
+
+        std::ifstream fs_in;
+        fs_in.open(file_path, std::ifstream::in);
+        if (!(fs_in.is_open())) {
+            // ignore unopenable files
+            continue;
+        }
+
+        mt_d_file_contents.push_back(read_file_noseek(fs_in));
+
+        fs_in.close();
+    }
+
 }
 
 #endif // _LIST_AND_READ_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
+#include <deque>
 #include <thread>
 #include <iostream>
 #include <filesystem>
@@ -8,32 +9,24 @@
 #include "../include/mt_deque.hpp"
 #include "../list_and_read/list_and_read.hpp"
 
+namespace fs = std::filesystem;
+
+typedef std::deque<char, std::allocator<char> > chardeque;
+
 int main() {
     std::string indir = "data/to_index";
     std::string out_by_a = "data/res_a.txt";
     std::string out_by_n = "data/res_n.txt";
     int indexing_threads = 5;
 
-    mt_deque_t<std::filesystem::path> mt_d;
+    mt_deque_t<std::filesystem::path> mt_d_filenames;
+    mt_deque_t<chardeque> mt_d_file_contents;
 
-    add_files_to_queue(mt_d, indir);
+    std::thread file_list_thread(add_files_to_queue, std::ref(mt_d_filenames), indir);
+    std::thread file_read_thread(add_files_to_queue, std::ref(mt_d_filenames), std::ref(mt_d_file_contents));
 
-    // mt_deque_t<int> mt_d;
-
-    // std::thread t2(&mt_deque_t<int>::push_back, &mt_d, 2);
-    // std::thread t3(&mt_deque_t<int>::push_front, &mt_d, 3);
-    // std::thread t1(&mt_deque_t<int>::push_back, &mt_d, 1);
-
-    // t2.join();
-    // t3.join();
-    // t1.join();
-
-    // // IT FUCKING WORKS!!!!!!!
-
-    // auto size = mt_d.size();
-    // for (size_t i = 0; i < size; i++) {
-    //     std::cout << mt_d.pop_back() << std::endl;
-    // }
+    file_list_thread.join();
+    file_read_thread.join();
 
 	return 0;
 }
