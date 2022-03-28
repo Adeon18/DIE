c07c8cf88d800768f51a3266313829210d0b223c
Adeon18 <ostaptrush18@gmail.com>
2022-03-28 10:13:53 +0300

    Added entire thread_not_safe unordered map

--- include/mt_unordered_map_t.h
+++ include/mt_unordered_map_t.h
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <iostream>
 #include <mutex>
+#include <iterator>
 #include <condition_variable>
 
 template<typename K, typename V>
@@ -13,17 +14,75 @@ public:
     ~mt_unordered_map_t() = default;
     mt_unordered_map_t(const mt_unordered_map_t&) = delete;
     mt_unordered_map_t& operator=(const mt_unordered_map_t&) = delete;
-    // TODO: Write these functions
-    void insert(std::pair<K, V>);
+
+    void insert(std::pair<K, V> pair);
     void erase(const K& key);
-    // TODO: What is noexcept?
     void clear() noexcept;
     V& operator[](const K& key);
+    // These functions are for iteration, they are protected by unique lock, but SHOUDN'T BE USED in multiple threads
+    typename std::unordered_map<K, V>::iterator begin() noexcept;
+    typename std::unordered_map<K, V>::iterator end() noexcept;
+
+    size_t size(); // Not to be called inside here because no recursive mutex here
 
 private:
-    std::unordered_map<K, V> map_m;
+    std::unordered_map<K, V> unordered_map_m;
     std::mutex mux_m;
     std::condition_variable cv_empty_m;
 };
+
+template<typename K, typename V>
+void mt_unordered_map_t<K, V>::insert(std::pair<K, V> pair) {
+    std::unique_lock<std::mutex> lock(mux_m);
+    unordered_map_m.insert(pair);
+#ifdef UNORDERED_MAP_DEBUG
+    std::cout << "insert:: Size(after call, pairs): " <<  unordered_map_m.size() << std::endl;
+#endif
+}
+
+template<typename K, typename V>
+void mt_unordered_map_t<K, V>::erase(const K &key) {
+    std::unique_lock<std::mutex> lock(mux_m);
+    unordered_map_m.erase(key);
+#ifdef UNORDERED_MAP_DEBUG
+    std::cout << "erase:: Size(after call, pairs): " <<  unordered_map_m.size() << "; Key: " << key << std::endl;
+#endif
+}
+
+template<typename K, typename V>
+void mt_unordered_map_t<K, V>::clear() noexcept {
+    std::unique_lock<std::mutex> lock(mux_m);
+    unordered_map_m.clear();
+#ifdef UNORDERED_MAP_DEBUG
+    std::cout << "clear:: Size(after call, pairs): " <<  unordered_map_m.size() << std::endl;
+#endif
+}
+
+template<typename K, typename V>
+V &mt_unordered_map_t<K, V>::operator[](const K &key) {
+    std::unique_lock<std::mutex> lock(mux_m);
+#ifdef UNORDERED_MAP_DEBUG
+    std::cout << "[] operator:: Size(before call, pairs): " <<  unordered_map_m.size() << "; Key: " << key << std::endl;
+#endif
+    return unordered_map_m[key];
+}
+
+template<typename K, typename V>
+typename std::unordered_map<K, V>::iterator mt_unordered_map_t<K, V>::begin() noexcept {
+    std::unique_lock<std::mutex> lock(mux_m);
+    return unordered_map_m.begin();
+}
+
+template<typename K, typename V>
+typename std::unordered_map<K, V>::iterator mt_unordered_map_t<K, V>::end() noexcept {
+    std::unique_lock<std::mutex> lock(mux_m);
+    return unordered_map_m.end();
+}
+
+template<typename K, typename V>
+size_t mt_unordered_map_t<K, V>::size() {
+    std::unique_lock<std::mutex> lock(mux_m);
+    return unordered_map_m.size();
+}
 
 #endif //TEMPLATE_MT_UNORDERED_MAP_T_H
--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
 
 #include <iostream>
 #include <thread>
+#include <array>
 #include "options_parser.h"
 #include "../include/mt_deque.hpp"
+#include "../include/mt_unordered_map_t.h"
 
 int main(int argv, char* argc[]) {
 
@@ -22,15 +24,41 @@ int main(int argv, char* argc[]) {
 
     // IT FUCKING WORKS!!!!!!!
 
-    for (int i = 0; i < 1000000; i++) {
-        mt_d.push_back(i);
+//    for (int i = 0; i < 1000000; i++) {
+//        mt_d.push_back(i);
+//    }
+//
+//    auto size = mt_d.size();
+//    std::cout << "size: " << size << std::endl;
+//    for (size_t i = 0; i < size; i++) {
+//        std::cout << mt_d.pop_back() << std::endl;
+//    }
+
+    mt_unordered_map_t<std::string , int> mt_um_t;
+
+    std::thread t2(&mt_unordered_map_t<std::string , int>::insert, &mt_um_t, std::pair<std::string, int>("Die", 2));
+    std::thread t3(&mt_unordered_map_t<std::string , int>::insert, &mt_um_t, std::pair<std::string, int>("Dead", 3));
+    std::thread t1(&mt_unordered_map_t<std::string , int>::insert, &mt_um_t, std::pair<std::string, int>("Depress", 1));
+    std::thread t4(&mt_unordered_map_t<std::string , int>::erase, &mt_um_t, "Depress");
+
+    t2.join();
+    t1.join();
+    t3.join();
+    t4.join();
+
+    std::string arr[3] = {"Die", "Dead", "Depress"};
+
+    mt_um_t["Depress"]++;
+
+    std::cout << mt_um_t.size() << std::endl;
+
+    for (const auto & item: mt_um_t) {
+        std::cout << item.first << " " << item.second << std::endl;
     }
 
-    auto size = mt_d.size();
-    std::cout << "size: " << size << std::endl;
-    for (size_t i = 0; i < size; i++) {
-        std::cout << mt_d.pop_back() << std::endl;
-    }
+    mt_um_t.clear();
+
+
 
 	return 0;
 }
