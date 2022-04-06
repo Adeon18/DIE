6387c2f8488346c5b18e5336b42fe167d14ce8a6
michael-2956 <work.michael.2956@gmail.com>
2022-04-06 16:27:04 +0300

    optimize reading

--- list_and_read/list_and_read.hpp
+++ list_and_read/list_and_read.hpp
@@ -13,14 +13,12 @@
 #include <array>
 
 namespace fs = std::filesystem;
-typedef std::deque<char, std::allocator<char> > chardeque;
 
 namespace list_and_read {
     void add_files_to_queue(
         mt_deque::mt_deque_t<fs::path>& mt_d_filenames, const std::string &indir,
         time_measurer::mt_time_summmator_t &time_sum
     ) {
-
         auto measure_start = time_measurer::get_current_time_fenced();
         for (auto const& dir_entry : fs::recursive_directory_iterator(indir)) {
             if (!dir_entry.path().empty()) mt_d_filenames.push_front(dir_entry.path());
@@ -31,30 +29,12 @@ namespace list_and_read {
         mt_d_filenames.push_front(fs::path(""));
     }
 
-    chardeque read_file_noseek(std::ifstream& fs_in) {
-        using std::begin;
-        using std::end;
-
-        // default buffer size of an ifstream
-        auto constexpr chunk_size = std::size_t{BUFSIZ};
-
-        auto container = std::deque<char>();
-
-        auto chunk = std::array<char, chunk_size>{};
-
-        while (
-            fs_in.read(chunk.data(), chunk.size()) || fs_in.gcount()
-        ) container.insert(
-            end(container), begin(chunk), begin(chunk) + fs_in.gcount()
-        );
-
-        return container;
-    }
-
     std::string read_file_fast(std::ifstream& fs_in) {
         auto const start_pos = fs_in.tellg();
         fs_in.ignore(std::numeric_limits<std::streamsize>::max());
         auto const char_count = fs_in.gcount();
+        if (char_count > static_cast<std::streamsize>(size_limits::FILE_SIZE_LIMIT_BYTES))
+            return "";
         fs_in.seekg(start_pos);
         auto s = std::string(char_count, char{});
         fs_in.read(&s[0], s.size());
@@ -65,9 +45,10 @@ namespace list_and_read {
         mt_deque::mt_deque_t<fs::path>& mt_d_filenames, mt_deque::mt_deque_t<std::string>& mt_d_file_contents,
         time_measurer::mt_time_summmator_t &time_sum
     ) {
-        auto measure_start = time_measurer::get_current_time_fenced();
         while (true) {
             auto file_path = mt_d_filenames.pop_back();
+
+            auto measure_start = time_measurer::get_current_time_fenced();
 
             [[unlikely]] if (file_path.empty()) {
                 mt_d_filenames.push_front(fs::path(""));
@@ -82,13 +63,14 @@ namespace list_and_read {
             }
 
             std::string file_contents_str = read_file_fast(fs_in);
-
-            if (!file_contents_str.empty() && file_contents_str.size() <= size_limits::FILE_SIZE_LIMIT_BYTES)
-                mt_d_file_contents.push_front(file_contents_str);
-
             fs_in.close();
+
+            // Compromise: intoducing error, but not counting the stalls.
+            time_sum.add_time(time_measurer::get_current_time_fenced() - measure_start);
+
+            if (!file_contents_str.empty())
+                mt_d_file_contents.push_front(file_contents_str);
         }
-        time_sum.add_time(time_measurer::get_current_time_fenced() - measure_start);
         mt_d_file_contents.push_front("");
     }
 }
