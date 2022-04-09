b5ae20fd7066b3471fdd2a94d49c29a3802562c7
michael-2956 <work.michael.2956@gmail.com>
2022-04-09 14:32:23 +0300

    fix find timing & noly .txt

--- list_and_read/list_and_read.hpp
+++ list_and_read/list_and_read.hpp
@@ -4,6 +4,7 @@
 #ifndef _LIST_AND_READ_HPP
 #define _LIST_AND_READ_HPP
 
+#include <boost/algorithm/string/predicate.hpp>
 #include "../time_measurer/time_measurer.hpp"
 #include "../mt_deque/mt_deque.hpp"
 #include "../include/limits.hpp"
@@ -14,6 +15,10 @@
 
 namespace fs = std::filesystem;
 
+namespace {
+    using boost::algorithm::ends_with;
+}
+
 namespace list_and_read {
     void add_files_to_queue(
         mt_deque::mt_deque_t<fs::path>& mt_d_filenames, const std::string &indir,
@@ -21,9 +26,9 @@ namespace list_and_read {
     ) {
         auto measure_start = time_measurer::get_current_time_fenced();
         for (auto const& dir_entry : fs::recursive_directory_iterator(indir)) {
-            if (!dir_entry.path().empty()) mt_d_filenames.push_front(dir_entry.path());
-
-            measure_start = time_measurer::get_current_time_fenced();
+            if (
+                !dir_entry.path().empty() && ends_with(dir_entry.path().string(), ".txt")
+            ) mt_d_filenames.push_front(dir_entry.path());
         }
         time_sum.add_time(time_measurer::get_current_time_fenced() - measure_start);
         mt_d_filenames.push_front(fs::path(""));
